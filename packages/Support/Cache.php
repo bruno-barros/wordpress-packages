750dfac2f6d517f57ddbb84d00ef8b81c67911d9
Bruno <dev@conceito-online.com.br>
2020-12-10 17:57:41 -0300

    cache tag support

--- packages/Support/Cache.php
+++ packages/Support/Cache.php
@@ -3,6 +3,7 @@
 use Closure;
 use Psr\Cache\InvalidArgumentException;
 use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+use Symfony\Component\Cache\Adapter\FilesystemTagAwareAdapter;
 use Symfony\Contracts\Cache\ItemInterface;
 
 /**
@@ -14,6 +15,7 @@ use Symfony\Contracts\Cache\ItemInterface;
 class Cache
 {
     public static $instance;
+    public static $tagInstance;
     
     private $cache;
     
@@ -26,15 +28,22 @@ class Cache
         $this->cache = $driver;
     }
     
-    
-    public static function make()
+    /**
+     * @param bool $tagSupport
+     * @return FilesystemAdapter|FilesystemTagAwareAdapter
+     */
+    public static function make($tagSupport = false)
     {
         if (is_null(self::$instance))
         {
-            self::$instance = new FilesystemAdapter('', 0, path('cache'));
+            self::$instance = new FilesystemAdapter('weloquent', 0, path('cache'));
+        }
+        if (is_null(self::$tagInstance))
+        {
+            self::$instance = new FilesystemTagAwareAdapter('weloquent', 0, path('cache'));
         }
         
-        return self::$instance;
+        return $tagSupport ? self::$tagInstance : self::$instance;
     }
     
     public static function get($key, $default = null)
@@ -64,7 +73,7 @@ class Cache
         {
             static::make()->get($key, function (ItemInterface $item) use ($value, $minutes) {
                 $item->expiresAfter((int)$minutes * 60);
-            
+                
                 return $value;
             });
         } catch (InvalidArgumentException $e)
@@ -72,15 +81,25 @@ class Cache
         }
     }
     
-    
-    public static function remember($key, $minutes, Closure $callback)
+    /**
+     * @param $key
+     * @param $minutes
+     * @param Closure $callback
+     * @param null|string|array $tags
+     * @return mixed
+     */
+    public static function remember($key, $minutes, Closure $callback, $tags = null)
     {
         try
         {
-            return static::make()->get($key, function (ItemInterface $item)
-            use ($callback, $minutes) {
+            return static::make((bool)$tags)->get($key, function (ItemInterface $item)
+            use ($callback, $minutes, $tags) {
                 $item->expiresAfter((int)$minutes * 60);
-            
+                if ($tags)
+                {
+                    $item->tag($tags);
+                }
+                
                 return $callback();
             });
         } catch (InvalidArgumentException $e)
@@ -122,6 +141,22 @@ class Cache
         }
     }
     
+    /**
+     * Remove an item from the cache.
+     *
+     * @param array $tags
+     * @return void
+     */
+    public static function forgetTags(array $tags)
+    {
+        try
+        {
+            static::make(true)->invalidateTags($tags);
+        } catch (InvalidArgumentException $e)
+        {
+        }
+    }
+    
     
     /**
      * Store an item in the cache if the key does not exist.
@@ -136,10 +171,11 @@ class Cache
     {
         /** @var FilesystemAdapter $adapter */
         $adapter = static::make();
-
+        
         if (!$adapter->hasItem($key))
         {
             static::put($key, $value, (int)$minutes);
+            
             return true;
         }
         
